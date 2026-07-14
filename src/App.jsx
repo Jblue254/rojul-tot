a9e1a9417242aa393db8f6a439e14c9c799542e9
Jblue254 <japhethkiprono2020@gmail.com>
2026-07-14 09:59:24 +0300

    feat : issue with shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Toaster } from 'sonner'
+import { cn } from "@/lib/utils";
+
 
 function App() {
   return (
     <>
-    <Toaster richColors position="top-right" />
+    {/* <Toaster richColors position="top-right" /> */}
+        <div className={cn("min-h-screen flex items-center justify-center bg-yellow-400")}>
+      <h1 className="text-5xl font-bold">
+        RojulTot
+      </h1>
+    </div>
 
     </>
   )
