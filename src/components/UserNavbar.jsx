2e9c1988e2b5fe7d22c3410a3ef5a8ec38779c31
Jblue254 <japhethkiprono2020@gmail.com>
2026-07-14 10:19:29 +0300

    feat:paths to the pages in the navbar to help in navigation

---
+++ src/components/UserNavbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function UserNavbar() {
+  const navLinks = [
+    { name: "Home", path: "/" },
+    { name: "About", path: "/about" },
+    { name: "Products", path: "/products" },
+    { name: "Gallery", path: "/gallery" },
+    { name: "Contact", path: "/contact" },
+  ];
+
+
+  return (
+    <div>User</div>
+  )
+}
+
+export default UserNavbar
+
+
