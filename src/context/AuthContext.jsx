47f7a0f4b3292fb9a76c959eac27a387515bd675
Jblue254 <japhethkiprono2020@gmail.com>
2026-07-15 13:22:00 +0300

    feat:variables to be used in the file auth

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -4,8 +4,63 @@ import { auth, db } from "@/services/firebase";
 import {createUserWithEmailAndPassword,signInWithEmailAndPassword,signOut,onAuthStateChanged,updateProfile,} from "firebase/auth";
 import {doc,setDoc,getDoc,serverTimestamp,} from "firebase/firestore";
 
+const AuthContext = createContext();
 
-function AuthContext() {
+export function AuthProvider({ children }) {
+
+  const [user, setUser] = useState(null);
+
+  const [loading, setLoading] = useState(true);
+
+
+  const register = async (name, email, password) => {
+    // Create Firebase Authentication account
+    const userCredential = await createUserWithEmailAndPassword(auth,email,password);
+
+    const firebaseUser = userCredential.user;
+
+    // Update Firebase Authentication profile
+    await updateProfile(firebaseUser, {displayName: name,});
+
+    
+    const personalDetails = {
+      uid: firebaseUser.uid,
+      name,
+      email,
+      role: "user",
+      wishlist: [],
+      createdAt: serverTimestamp(),
+    };
+
+    
+    await setDoc(
+      doc(db, "users", firebaseUser.uid),
+      personalDetails
+    );
+
+
+    setUser(personalDetails);
+
+    return firebaseUser;
+  };
+
+
+  const login = async (email, password) => {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+
+    const firebaseUser = userCredential.user;
+
+    const userDoc = await getDoc(
+      doc(db, "users", firebaseUser.uid)
+    );
+
+    if (userDoc.exists()) {
+      setUser(userDoc.data());
+    }
   return (
     <div>
 
