14a53b14bf88b422689a6beaedc1b7004bc8c85f
Jblue254 <japhethkiprono2020@gmail.com>
2026-07-15 13:12:55 +0300

    feat : imports needed in the auth context file

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import {createContext,useContext,useEffect,useState,} from "react";
+import { auth, db } from "@/services/firebase";
+import {createUserWithEmailAndPassword,signInWithEmailAndPassword,signOut,onAuthStateChanged,updateProfile,} from "firebase/auth";
+import {doc,setDoc,getDoc,serverTimestamp,} from "firebase/firestore";
+
 
 function AuthContext() {
   return (
     <div>
-        
+
     </div>
   )
 }
