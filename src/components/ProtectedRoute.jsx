814a512c4829ba3f6b1178b1127dcbdb5caac73b
Jblue254 <japhethkiprono2020@gmail.com>
2026-07-15 21:14:03 +0300

    feat: if you are not authenticated you are taken to the login page

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -5,6 +5,9 @@ import { useAuth } from "@/context/AuthContext";
 export default function ProtectedRoute({ adminOnly = false }) {
      const { isAuthenticated, isAdmin } = useAuth();
   const location = useLocation();
+  if (!isAuthenticated) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
   return (
     <div>
 
