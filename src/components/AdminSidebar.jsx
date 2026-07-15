76139bc6fd232c67414a3e7f90a5e898b683c09b
Jblue254 <japhethkiprono2020@gmail.com>
2026-07-15 23:03:08 +0300

    feat:neccesary imports for the file

--- src/components/AdminSidebar.jsx
+++ src/components/AdminSidebar.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { NavLink } from "react-router-dom";
+import {LayoutDashboard,Users,FileText,Truck,MessageSquare,LogOut} from "lucide-react";
 
 export default function AdminSidebar() {
   return (
