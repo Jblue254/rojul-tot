d85faffdb6dceb622b0fdbcbffab95744898f779
Jblue254 <japhethkiprono2020@gmail.com>
2026-07-15 21:12:27 +0300

    feat:importing the authcontext as a variable to be used in this file

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -3,6 +3,8 @@ import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useAuth } from "@/context/AuthContext";
 
 export default function ProtectedRoute({ adminOnly = false }) {
+     const { isAuthenticated, isAdmin } = useAuth();
+  const location = useLocation();
   return (
     <div>
 
