28f07e0a075fd276afbbeed3dd6c16eec655b017
Jblue254 <japhethkiprono2020@gmail.com>
2026-07-16 07:05:23 +0300

    feat:funtion for the usermanagement to help with the file page

--- src/pages/UserManagement.jsx
+++ src/pages/UserManagement.jsx
@@ -4,6 +4,27 @@ import { db } from "@/firebase";
 
 
 function UserManagement() {
+    onst [users, setUsers] = useState([]);
+
+  const fetchUsers = async () => {
+    try {
+      const snapshot = await getDocs(collection(db, "users"));
+
+      const userList = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      setUsers(userList);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
     <div>UserManagement</div>
   )
