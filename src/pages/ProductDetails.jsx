2286c4ca44d4ef21b25f5ffab7d6a71caf55f9a1
Jblue254 <japhethkiprono2020@gmail.com>
2026-07-16 23:16:54 +0300

    feat:imports nesseccary for the file

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "@/firebase";
+import UserNavbar from "@/components/UserNavbar";
+import Footer from "@/components/Footer";
 
 function ProductDetails() {
   return (
