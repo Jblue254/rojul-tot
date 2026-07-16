c73c7a8fed0e120f21c31f604ccb9d4be7ed2cc8
Jblue254 <japhethkiprono2020@gmail.com>
2026-07-16 22:07:54 +0300

    Feat:the head or main paragraph of the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,34 @@ import {
 
 function Footer() {
   return (
-  )
+   <footer className="bg-gray-900 text-gray-300">
+
+      <div className="max-w-7xl mx-auto px-6 py-16">
+
+        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-10">
+
+          {/* Company */}
+
+          <div>
+
+            <h2 className="text-3xl font-extrabold text-white">
+              Rojul
+              <span className="text-[#1495CC]">Tot</span>
+            </h2>
+
+            <p className="mt-4 leading-7 text-gray-400">
+              We Listen. Plan. Build.
+              <br /><br />
+              Providing reliable construction machinery,
+              architectural drawings, and quality construction
+              solutions for residential and commercial projects.
+            </p>
+
+          </div>
+
+         
+    </footer>
+  );
 }
 
 export default Footer;
